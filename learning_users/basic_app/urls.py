5c9635e8ee2bf5fb78d7e6931492d8efd366c428
Will VanderMerwe <will.vandermerwe@gmail.com>
2021-05-09 12:58:32 +0300

    first commit

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from basic_app import views
+
+# Template tags
+app_name = 'basic_app'
+
+urlpatterns=[
+    url(r'^registration/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+
+]
